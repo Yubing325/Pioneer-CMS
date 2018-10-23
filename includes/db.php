e61dfee6014f9fd9454be1e56a6925290995296a
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2018-10-23 10:25:42 -0500

    define 'db' array & toUpper the keys and values by foreach loop

--- includes/db.php
+++ includes/db.php
@@ -2,7 +2,17 @@
 // this is the database connection file
 // 2018-Oct-2
 
-$connection = mysqli_connect('localhost','root','','pioneer');
+$db['db_host'] = "localhost";
+$db['db_user'] ="root";
+$db['db_pass']="";
+$db['db_name']='pioneer';
+
+foreach($db as $key => $value){
+    define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+//$connection = mysqli_connect('localhost','root','','pioneer');
 
 if($connection){
     echo "connected!";
